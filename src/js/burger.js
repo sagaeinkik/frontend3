10c125415eea644d596237e2903db5fe66a1d707
Saga Einarsdotter Kikajon <saga@kikajon.se>
2024-02-06 15:22:19 +0100

    Logotyp byts ut vid aktivt hamburgerläge i mörkt tema

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,8 +1,22 @@
 'use strict';
-const burgerButton = document.querySelector('.burger-button');
-const mobileMenu = document.querySelector('.mobile-menu-items');
+//Variabler
+const burgerButton = document.querySelector('.burger-button'); //Själva burgarknappen
+const mobileMenu = document.querySelector('.mobile-menu-items'); //Själva ul-elementet med menyns innehåll
+const mobileNav = document.querySelector('div.mobile-menu');
 
+//Vid klick på knappen, toggla klass active på meny och knapp
 burgerButton.addEventListener('click', () => {
     mobileMenu.classList.toggle('active');
     burgerButton.classList.toggle('active');
+});
+
+//Stäng menyn vid klick utanför
+document.addEventListener('click', (e) => {
+    //Lagra om klicket skedde inuti hela menyområdet
+    const clickInside = mobileNav.contains(e.target);
+    if (!clickInside) {
+        //Om nej, ta bort active från meny och knapp
+        burgerButton.classList.remove('active');
+        mobileMenu.classList.remove('active');
+    }
 });
---
+++ src/js/colorscheme.js
@@ -0,0 +1,36 @@
+'use strict';
+/* Det jag vill åstadkomma: 
+- Ljust tema + inaktiv hamburgermeny = mörk logotyp
+- Ljust tema + aktiv hamburgermeny = mörk logotyp
+- Mörkt tema + inaktiv hamburgermeny = mörk logotyp
+- Mörkt tema + aktiv hamburgermeny = ljus logotyp */
+
+// Hämta sökvägarna till bilderna med import för att parcel ställer till det med vanliga sökvägarna
+import logotypeLight from '../images/logo/signature-light.svg';
+import logotypeDark from '../images/logo/signature-black.svg';
+
+//Variabler
+const burgerButton = document.querySelector('.burger-button'); //Burgarknappen, för att kolla om aktiv
+const logotype = document.getElementById('logotyp'); //Logotyp-elementet, för att byta ut src-attribut
+const darkModeQuery = window.matchMedia('(prefers-color-scheme: dark)'); //Kolla om dark mode är på
+
+function updateLogotype() {
+    //Villkor: kolla om dark mode är på
+    if (darkModeQuery.matches) {
+        //Om ja, kolla om burgaren har klassen active
+        if (burgerButton.classList.contains('active')) {
+            //Om ja, byt logotyp
+            logotype.src = logotypeLight;
+        } else {
+            //Om burgaren är inaktiv, byt tillbaka till mörk
+            logotype.src = logotypeDark;
+        }
+    } else {
+        //Om dark mode är av, se till att loggan är mörk
+        logotype.src = logotypeDark;
+    }
+}
+
+updateLogotype(); //Kalla funktionen direkt
+darkModeQuery.addEventListener('change', updateLogotype); //Kalla funktionen om tema byts (så man inte behöver refresha)
+burgerButton.addEventListener('click', updateLogotype); //Kalla funktionen om man klickar på burger, för då ändras ju klassen
