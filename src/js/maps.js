b6a06d937e1f799d07f9a1a9ad7dee29554db1eb
Saga Einarsdotter Kikajon <saga@kikajon.se>
2024-02-24 14:07:21 +0100

    Kodkommentarer, småfix

--- src/js/maps.js
+++ src/js/maps.js
@@ -1,18 +1,22 @@
 'use strict';
 
+/* Importera leaflet-komponenter */
 import L from 'leaflet';
 
 import 'leaflet/dist/leaflet.css';
+/* Importera controller från maptiler API */
 import { GeocodingControl } from '@maptiler/geocoding-control/leaflet';
 import '@maptiler/geocoding-control/style.css';
 
+/* Skapa kartan (Baspunkt Åre) */
 const map = L.map('map', {
     center: L.latLng(63.402326, 13.078144),
     zoom: 14,
 });
-
+/* API-nyckel till Maptiler */
 const key = 'ywKVXMnPP04HAr2z7kvP';
 
+/* Tiles från maptiler */
 L.tileLayer(
     `https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=${key}`,
     {
@@ -26,4 +30,12 @@ L.tileLayer(
     }
 ).addTo(map);
 
-L.control.maptilerGeocoding({ apiKey: key }).addTo(map);
+/* Sökfunktion från Maptiler Geocoding API */
+L.control
+    .maptilerGeocoding({
+        apiKey: key,
+        placeholder: 'Sök',
+        collapsed: true,
+        language: 'sv',
+    })
+    .addTo(map);
