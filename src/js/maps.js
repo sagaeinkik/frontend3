39bd630cc6d28f35b92528ccfb1354f576543380
Saga Einarsdotter Kikajon <saga@kikajon.se>
2024-02-26 12:06:55 +0100

    Ändrat sökfunktion

--- src/js/maps.js
+++ src/js/maps.js
@@ -2,11 +2,15 @@
 
 /* Importera leaflet-komponenter */
 import L from 'leaflet';
-
 import 'leaflet/dist/leaflet.css';
-/* Importera controller från maptiler API */
-import { GeocodingControl } from '@maptiler/geocoding-control/leaflet';
-import '@maptiler/geocoding-control/style.css';
+import markerIcon from '../images/marker-icon.png';
+/* Skapa markern */
+const marker = L.icon({
+    iconUrl: markerIcon,
+    iconAnchor: [13, 40],
+});
+/* Position null så att man kan ta bort gamla markern */
+let position = null;
 
 /* Skapa kartan (Baspunkt Åre) */
 const map = L.map('map', {
@@ -30,12 +34,68 @@ L.tileLayer(
     }
 ).addTo(map);
 
-/* Sökfunktion från Maptiler Geocoding API */
-L.control
-    .maptilerGeocoding({
-        apiKey: key,
-        placeholder: 'Sök',
-        collapsed: true,
-        language: 'sv',
-    })
-    .addTo(map);
+/* Sökfunktion */
+const searchField = document.getElementById('search');
+const searchButton = document.querySelector('div.search button');
+const searchResults = document.querySelector('div.searchresults ul');
+
+searchButton.addEventListener('click', searchLocation);
+searchField.addEventListener('keyup', (e) => {
+    // Om man trycker enter
+    if (e.key === 'Enter') {
+        searchLocation();
+    }
+});
+
+/* Funktion som gör fetchanrop till nominatim API */
+async function searchLocation() {
+    const searchString = searchField.value;
+    const apiURL =
+        'https://nominatim.openstreetmap.org/search?format=json&q=' +
+        encodeURIComponent(searchString);
+    try {
+        const response = await fetch(apiURL);
+        const data = await response.json();
+        //Skicka resultaten vidare
+        displayResults(data);
+    } catch (error) {
+        console.error('Fel: ' + error);
+    }
+}
+
+/* Funktion som skriver ut sökresultat på skärmen */
+function displayResults(data) {
+    searchResults.innerHTML = '';
+    /* kontroll om sökning lyckades */
+    if (data) {
+        // forEach som skapar nytt li-element för varje resultat
+        data.forEach((result) => {
+            const listItem = document.createElement('li');
+            //lägg till longitud och latitud som dataset så man kan komma åt från annan funktion
+            listItem.setAttribute('data-lat', result.lat);
+            listItem.setAttribute('data-long', result.lon);
+            const textResult = document.createTextNode(result.display_name);
+            //peta in i list items och in i lista
+            listItem.appendChild(textResult);
+            searchResults.appendChild(listItem);
+
+            //event listener på varje, som hämtar ut lat/long från datasets och skickar med till nästa funktion
+            listItem.addEventListener('click', (e) => {
+                const lat = e.target.dataset.lat;
+                const long = e.target.dataset.long;
+
+                moveMap(lat, long);
+            });
+        });
+    }
+}
+
+/* Funktion som flyttar karta och markering */
+function moveMap(lat, long) {
+    if (position !== null) {
+        map.removeLayer(position);
+    }
+    map.removeLayer(marker);
+    map.flyTo(L.latLng(lat, long));
+    position = L.marker([lat, long], { icon: marker }).addTo(map);
+}
